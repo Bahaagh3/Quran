6be2e94425eaf380067e4fe98e55eba9cb950146
bahaaalg <bahaaalg@uio.no>
2024-01-17 16:54:14 +0100

    finshed with sidebar and reading page

--- SurahPage.js
+++ SurahPage.js
@@ -2,7 +2,19 @@ function getQueryParam(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
-const surahId = getQueryParam('surah');
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+let surahId = getQueryParam('surah');
+if (surahId == null){
+    surahId =1
+}
+
 let test = new Request('https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number='+surahId)
 fetch(test)
 .then(response => {
@@ -13,7 +25,14 @@ fetch(test)
   })
 .then(data => {
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 const verses = data["verses"]; 
 let parent = document.getElementById("0")
 console.log(verses)
@@ -24,10 +43,23 @@ for (let i = 0; i<verses.length;i++){
     newDiv.className = "ayah"
     newDiv.textContent= content
     let newInfo = document.createElement("p")
-    newInfo.className = "info"
+    newInfo.className = "ayahinfo"
     newInfo.textContent = key
     newDiv.appendChild(newInfo)
     parent.appendChild(newDiv)
 }
 
 })
+fetch('https://api.alquran.cloud/v1/meta')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById("mySidenav");
+        const surahs = data.data.surahs.references;
+        surahs.forEach(surah => {
+            const surahElement =document.createElement("a")
+            surahElement.href = "SurahPage.html?surah="+surah.number
+            surahElement.textContent = surah.name
+            container.appendChild(surahElement)
+        });
+    })
+    .catch(error => console.error('Error fetching Surah data:', error));
