fe53b2778bdf591564a65fa9729d3cb9300b04c1
bahaaalg <bahaaalg@uio.no>
2024-01-17 15:02:02 +0100

    finshed with api and surahs

--- SurahPage.js
+++ SurahPage.js
@@ -3,7 +3,7 @@ function getQueryParam(name) {
     return urlParams.get(name);
 }
 const surahId = getQueryParam('surah');
-let test = new Request('https://api.quran.com/api/v4/quran/verses/indopak?chapter_number='+surahId)
+let test = new Request('https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number='+surahId)
 fetch(test)
 .then(response => {
     if (!response.ok) {
@@ -15,7 +15,19 @@ fetch(test)
 
 
 const verses = data["verses"]; 
+let parent = document.getElementById("0")
 console.log(verses)
-
+for (let i = 0; i<verses.length;i++){
+    let content = verses[i]["text_uthmani"]//.replaceAll("","").replaceAll("","").replaceAll("","").replaceAll("","")
+    let key  = verses[i]["verse_key"]
+    let newDiv = document.createElement("div")
+    newDiv.className = "ayah"
+    newDiv.textContent= content
+    let newInfo = document.createElement("p")
+    newInfo.className = "info"
+    newInfo.textContent = key
+    newDiv.appendChild(newInfo)
+    parent.appendChild(newDiv)
+}
 
 })
